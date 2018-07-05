fe0c9b19d15de8af96663095e2d106b759c86932
Ahmad Anvari <ahmad.anv@gmail.com>
2018-07-05 15:03:16 +0430

    Adding new source file to the project

---
+++ bitcoin.srcs/sources_1/new/padder.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/05/2018 02:58:22 PM
+-- Design Name: 
+-- Module Name: PADDER - BEHAVIORAL
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PADDER is
+    Port ( MSG : in STD_LOGIC_VECTOR (1024 downto 0));
+end PADDER;
+
+architecture BEHAVIORAL of PADDER is
+
+begin
+
+
+end BEHAVIORAL;
